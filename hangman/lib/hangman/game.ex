97535a0eeeea7bcdca84d29d52449ea5bfe8d9bf
Cristian Riano <cristiancarm11@gmail.com>
2020-05-09 12:57:37 +0200

    Handlong of already won or lost games

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -1,5 +1,4 @@
 defmodule Hangman.Game do
-
   defstruct(
     turns_left: 7,
     game_state: :initializing,
@@ -8,7 +7,16 @@ defmodule Hangman.Game do
 
   def new_game() do
     %Hangman.Game{
-      letters: Dictionary.random_word |> String.codepoints
+      letters: Dictionary.random_word() |> String.codepoints()
     }
+  end
+
+  def make_move(game = %{game_state: state}, _guess)
+      when state in [:won, :lost] do
+        { game, tally(game) }
+  end
+
+  def tally(_game) do
+    123
   end
 end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -10,4 +10,19 @@ defmodule GameTest do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
   end
+
+  test "new_game letters are all ASCII characters" do
+    letters = Game.new_game().letters
+    regex = ~r/[a-z]/
+
+    letters
+      |> Enum.each(fn x -> assert(Regex.match?(regex, x)) end)
+  end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "a")
+    end
+  end
 end
