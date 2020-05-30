4b799b370eb54ba68db8b68afddcaf6814dd54d7
Cristian Riano <cristiancarm11@gmail.com>
2020-05-31 00:02:18 +0200

    Move Hangman into a GenServer for every new game

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -22,50 +22,55 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [:won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert ^game = Game.make_move(game, "a")
+      assert { ^game, _ } = Game.make_move(game, "a")
     end
   end
 
-  test "a invalid guess is flagged" do
-    game = Game.new_game() |> Game.make_move(1)
+  test "guesses can't be numbers" do
+    { game, _ } = Game.new_game() |> Game.make_move("1")
+    assert game.game_state == :invalid_guess
+  end
+
+  test "guesses can't be multiple letters" do
+    { game, _ } = Game.new_game() |> Game.make_move("ar")
     assert game.game_state == :invalid_guess
   end
 
   test "first occurrence of letter is not marked as already_used" do
-    game = Game.new_game() |> Game.make_move("x")
+    { game, _ } = Game.new_game() |> Game.make_move("x")
     assert game.game_state != :already_used
   end
 
   test "later occurrence of letter is marked as already_used" do
-    game = Game.new_game() |> Game.make_move("x")
+    { game, _ } = Game.new_game() |> Game.make_move("x")
     assert game.game_state != :already_used
 
-    game = Game.make_move(game, "x")
+    { game, _ } = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
-    game = Game.new_game("yes") |> Game.make_move("y")
+    { game, _ } = Game.new_game("yes") |> Game.make_move("y")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a guessed word is a won" do
-    game = Game.new_game("yes") |> Game.make_move("y")
+    { game, _ } = Game.new_game("yes") |> Game.make_move("y")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
 
-    game = Game.make_move(game, "e")
+    { game, _ } = Game.make_move(game, "e")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
 
-    game = Game.make_move(game, "s")
+      { game, _ } = Game.make_move(game, "s")
     assert game.game_state == :won
     assert game.turns_left == 7
   end
 
   test "a bad guess is recognized" do
-    game = Game.new_game("yes") |> Game.make_move("x")
+    { game, _ } = Game.new_game("yes") |> Game.make_move("x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
@@ -81,7 +86,7 @@ defmodule GameTest do
       { "g", :lost },
     ] |> Enum.with_index()
       |> Enum.reduce(Game.new_game("x"), fn {{move, state}, index}, game ->
-        game = Game.make_move(game, move)
+        { game, _ } = Game.make_move(game, move)
         assert game.game_state == state
         assert game.turns_left == 7 - (index + 1)
 
