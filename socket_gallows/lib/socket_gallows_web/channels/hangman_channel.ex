e7bc0c196ee8ed98e48f647ab50077497fe0de53
Cristian Riano <cristiancarm11@gmail.com>
2020-07-10 15:26:49 +0200

    Retrieve the tally via websocket

--- socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
+++ socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
@@ -1,7 +1,23 @@
 defmodule SocketGallowsWeb.HangmanChannel do
+  require Logger
+
   use Phoenix.Channel
 
   def join("hangman:game", _, socket) do
+    game = Hangman.new_game()
+    socket = assign(socket, :game, game)
     {:ok, socket}
+  end
+
+  def handle_in("tally", _, socket) do
+    game = socket.assigns.game
+    tally = Hangman.tally(game)
+    push(socket, "tally", tally)
+    {:noreply, socket}
+  end
+
+  def handle_in(msg, _, socket) do
+    Logger.error("Unknown message #{msg}")
+    {:noreply, socket}
   end
 end
